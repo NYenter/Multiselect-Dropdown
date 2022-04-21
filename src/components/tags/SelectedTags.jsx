97e1f4ea862a586fe2853e1df5f8f462ce4342b2
Nick <nick17broncos@gmail.com>
2022-04-21 00:07:58 -0500

    Verfied it worked after cloning

--- src/components/tags/SelectedTags.jsx
+++ src/components/tags/SelectedTags.jsx
@@ -12,7 +12,6 @@ const SelectedTags = ({ items, removeItem }, ref) => {
           key={item.name}
           item={item}
           removeItem={removeItem}
-          onKeyDownCapture={handleTabPressed}
         />
       ))}
     </TagContainer>
